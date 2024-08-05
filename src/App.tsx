d26077318490a083a72157ff4057694b1f4377b5
margi.shah1 <margi.shah@smartsensesolutions.com>
2024-08-05 14:37:48 +0530

    feat: add tooltip code

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Tooltip } from './components/Tooltip/Tooltip.component';
+import { tooltip } from './App.helper';
+
+function App() {
+    return (
+        <>
+            <h3>Tooltip Demo</h3>
+            <div className="container">
+                {tooltip.map((tlp, index) => (
+                    <Tooltip
+                        title={tlp.tooltipLabel}
+                        arrow={tlp.arrow}
+                        key={index}
+                        tooltipPosition={tlp.tooltipPosition}
+                        tooltipOnOverflow={tlp.tooltipOnOverflow}
+                    >
+                        <div className={tlp.className}>{tlp.title}</div>
+                    </Tooltip>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default App;

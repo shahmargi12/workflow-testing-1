f39bd0b780e2f7633cb3558b3496231e7da6fdcb
margi.shah1 <margi.shah@smartsensesolutions.com>
2024-08-06 15:29:16 +0530

    feat: update message

--- src/App.helper.tsx
+++ src/App.helper.tsx
@@ -17,7 +17,7 @@ export const tooltipLabel = (
         <br />
         This is why it is displayed below instead! This tooltip does not fit above the button.
         <br />
-        This is why it's displayed below instead!
+        This is why it is displayed below instead!
     </div>
 );
 
